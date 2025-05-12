d5db3a8ef6a27d26a340c7c88774bea253f09f26
jack-2828 <zcy4pf@virginia.edu>
2025-05-12 16:17:53 -0400

    improved code reusability + refractored

--- access-data/download-stock-data.py
+++ access-data/download-stock-data.py
@@ -3,97 +3,91 @@ import pandas as pd
 from datetime import timedelta
 import datetime
 import os
-current_date = datetime.datetime.today().strftime("%m-%d-%Y")
-# incrementing the date by 1 day to ensure today's data is included
-tomorrow_date = datetime.datetime.today() + timedelta(days=1)
-tomorrow_date = tomorrow_date.strftime("%Y-%m-%d")
+import asyncio
 
-for num in range(3):
-    if num == 0:
-        dat_data = yf.Ticker('QQQ').history(period='1d', start='2024-01-01', end=tomorrow_date)
-        name = "QQQ"
+
+class GenerateData():
+    def __init__(self):
+        self.current_date = datetime.datetime.today().strftime("%m-%d-%Y")
+        # incrementing the date by 1 day to ensure today's data is included
+        self.tomorrow_date = datetime.datetime.today() + timedelta(days=1)
+        self.tomorrow_date = self.tomorrow_date.strftime("%Y-%m-%d")
+    def create_no_gap(self):
+        for num in range(3):
+            if num == 0:
+                dat_data = self.get_stock_data("QQQ")
+                self.name = "QQQ"
+            else:
+                if num == 1:
+                    dat1_data = self.get_stock_data('XLY')
+                    dat2_data = self.get_stock_data('XLP')
+                    self.name = "XLY-vs-XLP"
+                elif num == 2:
+                    dat1_data = self.get_stock_data('RSPD')
+                    dat2_data = self.get_stock_data('RSPS')
+                    self.name = "RSPD-vs-RSPS"
+                ratio = pd.DataFrame()
+                for (index1, row1), (index2, row2) in zip(dat1_data.iterrows(), dat2_data.iterrows()):
+                    if (index1 != index2):
+                        raise Exception("Stopping... dates in ratio don't match")
+                    new_ratio_row = pd.DataFrame({
+                        'Open':  [round(row1["Open"] / row2["Open"], 4)],
+                        'High':  [0],
+                        'Low':   [0],
+                        'Close': [round(row1['Close'] / row2["Close"], 4)]
+                    }, index=[index1])
+                    ratio = pd.concat([ratio, new_ratio_row], axis=0)
+                
+                dat_data = ratio.copy(deep=True)
+            no_gaps_data = pd.DataFrame()
+            # no_gaps_data.index.name = 'Date'
+            previous_close = 0
+            for index, row in dat_data.iterrows():
+                if previous_close == 0:
+                    new_row = pd.DataFrame({
+                        'Open':  [round(row['Open'], 4)],
+                        'High':  [0],
+                        'Low':   [0],
+                        'Close': [round(row['Close'], 4)]
+                    }, index=[index])
+                    previous_close = row['Close']
+                else:
+                    adj_open = round(previous_close, 4)
+                    adj_close = round((row['Close'] - row['Open']) + adj_open, 4)
+                    new_row = pd.DataFrame({
+                        'Open': [adj_open],
+                        'High': [0],
+                        'Low': [0],
+                        'Close': [adj_close]
+                        }, index=[index])
+                    previous_close = adj_close
+                no_gaps_data = pd.concat([no_gaps_data, new_row], axis=0)
+            self.save_data(no_gaps_data)
+    def get_stock_data(self, ticker: str) -> pd.DataFrame:
+        dat_data = yf.Ticker(ticker).history(period='1d', start='2024-01-01', end=self.tomorrow_date)
         dat_data.drop(columns=['Dividends', 'Stock Splits', "Capital Gains"], inplace=True)
         dat_data.index = pd.to_datetime(dat_data.index)
         dat_data.index = dat_data.index.strftime('%m-%d-%Y')
-    else:
-        if num == 1:
-            dat1_data = yf.Ticker('XLY').history(period='1d', start='2020-01-01', end=tomorrow_date)
-            dat2_data = yf.Ticker('XLP').history(period='1d', start='2020-01-01', end=tomorrow_date)
-             # save these files to csv
-            with open('XLY_raw_data.csv', 'w', newline='') as f:
-                dat1_data.to_csv(f)
-                f.close()
-            with open('XLP_raw_data.csv', 'w', newline='') as f:
-                dat2_data.to_csv(f)
-                f.close()
-            name = "XLY-vs-XLP"
-        elif num == 2:
-            dat1_data = yf.Ticker('RSPD').history(period='1d', start='2024-01-01', end=tomorrow_date)
-            dat2_data = yf.Ticker('RSPS').history(period='1d', start='2024-01-01', end=tomorrow_date)
-            name = "RSPD-vs-RSPS"
-        dat1_data.drop(columns=['Dividends', 'Stock Splits', "Capital Gains"], inplace=True)
-        dat1_data.index = pd.to_datetime(dat1_data.index)
-        dat1_data.index = dat1_data.index.strftime('%m-%d-%Y')
-        dat2_data.drop(columns=['Dividends', 'Stock Splits', "Capital Gains"], inplace=True)
-        dat2_data.index = pd.to_datetime(dat2_data.index)
-        dat2_data.index = dat2_data.index.strftime('%m-%d-%Y')
-        ratio = pd.DataFrame()
-        counter = 0
-        for (index1, row1), (index2, row2) in zip(dat1_data.iterrows(), dat2_data.iterrows()):
-            counter += 1
-            if (index1 != index2):
-                raise Exception("Stopping... dates in ratio don't match")
-            new_ratio_row = pd.DataFrame({
-                'Open':  [round(row1["Open"] / row2["Open"], 3)],
-                'High':  [0],
-                'Low':   [0],
-                'Close': [round(row1['Close'] / row2["Close"], 3)]
-            }, index=[index1])
-            if counter < 10:
-                print(counter)
-                print(f'XLY / XLP : {index1} | {index2} {row1["Open"]} / {row2["Open"]}')
+        return dat_data
+    def save_data(self, df: pd.DataFrame):
+        '''
+        Save the given no-gap pandas dataframe to disk as a .csv
 
-            ratio = pd.concat([ratio, new_ratio_row], axis=0)
-        
-        dat_data = ratio.copy(deep=True)
-        with open(f'{name} ratio test.csv', 'w', newline="") as f:
-            dat_data.to_csv(f)
+        df: The data-frame to be saved as a .csv
+        '''
+        home = os.path.expanduser("~")
+        desktop = os.path.join(home, "Desktop")
+        target_folder = os.path.join(desktop, "StockCharts-User-Defined-Indexes")
+        os.makedirs(target_folder, exist_ok=True)
+        file_path = os.path.join(target_folder, f'{self.name}_no_gaps-{self.current_date}.csv')
+        try: 
+            # implement async file saving to speed up process
+            with open(file_path, 'w', newline="") as f:
+                df.to_csv(f, index_label="Date")
+            print("File saved at", file_path)
+        except:
+            print("An error was encountered saving", file_path)
 
-    
-
-    qqq_no_gaps = pd.DataFrame()
-    qqq_no_gaps.index.name = 'Date'
-    previous_close = 0
-    for index, row in dat_data.iterrows():
-        if previous_close == 0:
-            new_row = pd.DataFrame({
-                'Open':  [round(row['Open'], 2)],
-                'High':  [0],
-                'Low':   [0],
-                'Close': [round(row['Close'], 2)]
-            }, index=[index])
-            previous_close = row['Close']
-        else:
-            adj_open = round(previous_close, 2)
-            adj_close = round((row['Close'] - row['Open']) + adj_open, 2)
-            new_row = pd.DataFrame({
-                'Open': [adj_open],
-                'High': [0],
-                'Low': [0],
-                'Close': [adj_close]
-                }, index=[index])
-            previous_close = adj_close
-        qqq_no_gaps = pd.concat([qqq_no_gaps, new_row], axis=0)
-
-    home = os.path.expanduser("~")
-    desktop = os.path.join(home, "Desktop")
-    target_folder = os.path.join(desktop, "StockCharts-User-Defined-Indexes")
-    os.makedirs(target_folder, exist_ok=True)
-    file_path = os.path.join(target_folder, f'{name}_no_gaps-{current_date}.csv')
-    try: 
-        # implement async file saving to speed up process
-        with open(file_path, 'w', newline="") as f:
-            qqq_no_gaps.to_csv(f, index_label="Date")
-        print("File saved at", file_path)
-    except:
-        print("An error was encountered saving", file_path)
+if __name__ == "__main__":
+    get_data = GenerateData()
+    get_data.create_no_gap()
